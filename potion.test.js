bf65c863ff8e33629038ee9340148b1a32183bb6
cinful79 <cinful79@gmail.com>
2020-09-04 12:04:48 -0400

    updates

---
+++ potion.test.js
@@ -0,0 +1,8 @@
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+  
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+  });
+
+  
